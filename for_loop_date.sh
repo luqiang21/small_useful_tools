c12baf03c5280342f6520755108438a4c80d6d64
Qiang Lu <buaaluqiang@hotmail.com>
2017-10-16 15:59:23 -0600

    add for loop on date for bash

---
+++ for_loop_date.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 'start date = ' $1
+echo 'end date = ' $2
+
+start=`date -j -f %m/%d/%Y:%H.%M $1:0.0 +%s`
+enddate=`date -j -f %m/%d/%Y:%H.%M $2:0.0 +%s`
+
+echo
+
+for((i=$start; i<=$((enddate-86400)); i+=86400)) ; do
+	begin=`date -j -f %s "$i" +%m/%d/%Y`
+	End=`date -j -f %s "$((i+86400))" +%m/%d/%Y`
+
+	echo 'from start_date: ' $begin ' to end_date: ' $End
+done;
+
